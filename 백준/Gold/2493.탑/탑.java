a954fcb5c94e96e169dc41f0e5d3e3066f02e4ee
Chanbi Bae <73926213+chanbi428@users.noreply.github.com>
2022-02-07 19:15:39 +0900

    [Gold V] Title: 탑, Time: 2956 ms, Memory: 165128 KB -BaekjoonHub

--- 백준/Gold/2493.탑/탑.java
+++ 백준/Gold/2493.탑/탑.java
@@ -3,19 +3,12 @@ import java.io.*;
 
 
 public class Main {	
-	static class Top{
-		int h, index;
-		Top(int h, int index){
-			this.h = h;
-			this.index = index;
-		}
-	}
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
 		int[] answer = new int[n];
-		Stack<Top> stack = new Stack<>();
+		Stack<Integer> stack = new Stack<>();
 		int[] tops = new int[n];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<n; i++) {
@@ -24,13 +17,13 @@ public class Main {
 		
 		for(int i=n-1; i>=0; i--) {
 			while(!stack.empty()) {
-				if(stack.peek().h<tops[i]) {
-					answer[stack.peek().index] = i+1;
+				if(tops[stack.peek()]<tops[i]) {
+					answer[stack.peek()] = i+1;
 					stack.pop();
 				} 
 				else break;
 			}
-			stack.push(new Top(tops[i], i));
+			stack.push(i);
 		}
 		for(int i=0; i<n; i++) System.out.print(answer[i]+" ");
 		
