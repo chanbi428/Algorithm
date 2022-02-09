aa43216e128c4233208d69f93ad741683d2b6ea1
Chanbi Bae <73926213+chanbi428@users.noreply.github.com>
2022-02-09 10:13:41 +0900

    [Gold V] Title: 탑, Time: 524 ms, Memory: 106316 KB -BaekjoonHub

--- 백준/Gold/2493.탑/탑.java
+++ 백준/Gold/2493.탑/탑.java
@@ -1,28 +1,43 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
-
-public class Main {	
-	public static void main(String[] args) throws Exception {
+public class Main {
+	public static int N;
+	public static ArrayDeque<top> stack = new ArrayDeque<>();
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
-		int n = Integer.parseInt(br.readLine());
-		Stack<int[]> stack = new Stack<>();
+		N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=1; i<=n; i++) {
-			int height = Integer.parseInt(st.nextToken());
-			while(!stack.empty()) {
-				if(stack.peek()[1]>=height) {
-					sb.append(stack.peek()[0]).append(" ");
+		for (int i = 0; i < N; i++) {
+			int n = Integer.parseInt(st.nextToken());
+			while(!stack.isEmpty()) {
+				if(stack.peek().num < n){
+					stack.pop();
+				}else {
+					sb.append(stack.peek().count).append(" ");
 					break;
-				} 
-				stack.pop();
+				}
 			}
-			if(stack.empty()) sb.append("0 ");
-			stack.push(new int[] {i, height});
+			if(stack.isEmpty()) {
+				sb.append(0).append(" ");
+			}
+			stack.push(new top(n,i+1));
 		}
 		System.out.println(sb);
-		
 	}
+	
+}
+class top{
+	int num;
+	int count;
+	
+	public top(int num, int count) {
+		this.num = num;
+		this.count = count;
+	}
+
 }
