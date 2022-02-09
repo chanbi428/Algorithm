74f8e03dbf20233dfd91bd0ef55e751b93f9ecc4
Chanbi Bae <73926213+chanbi428@users.noreply.github.com>
2022-02-09 10:08:53 +0900

    [Gold V] Title: 탑, Time: 632 ms, Memory: 108212 KB -BaekjoonHub

--- 백준/Gold/2493.탑/탑.java
+++ 백준/Gold/2493.탑/탑.java
@@ -13,13 +13,13 @@ public class Main {
 		for(int i=1; i<=n; i++) {
 			int height = Integer.parseInt(st.nextToken());
 			while(!stack.empty()) {
-				if(stack.peek()[1]<height) {
-					stack.pop();
+				if(stack.peek()[1]>=height) {
+					sb.append(stack.peek()[0]).append(" ");
+					break;
 				} 
-				else break;
+				stack.pop();
 			}
-			if(!stack.empty()) sb.append(stack.peek()[0]+" ");
-			else sb.append("0 ");
+			if(stack.empty()) sb.append("0 ");
 			stack.push(new int[] {i, height});
 		}
 		System.out.println(sb);
