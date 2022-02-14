83c5a62ae355695c47a05413c74d2c0bf168d052
Chanbi Bae <73926213+chanbi428@users.noreply.github.com>
2022-02-14 20:11:48 +0900

    [Silver III] Title: 크면서 작은 수, Time: 76 ms, Memory: 11392 KB -BaekjoonHub

---
+++ 백준/Silver/2992.크면서 작은 수/크면서 작은 수.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+public class Main {
+	
+	static int[] num;
+	
+	static boolean np() {
+		int[] src = num;
+		
+		int i = src.length - 1;
+		while(i>0 && src[i-1]>=src[i]) --i;
+		
+		if(i==0) return false;
+		
+		int j = src.length -1;
+		while(src[i-1] >= src[j]) --j;
+		swap(src, i-1, j);
+		
+		int k = src.length - 1;
+		while(i<k) {
+			swap(src, i++, k--);
+		}
+		return true;
+	}
+	
+	static void swap(int[] array, int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		num = new int[str.length()];
+		for(int i=0; i<str.length(); i++) num[i] = str.charAt(i)-'0';
+		if(np()) {
+			StringBuilder answer = new StringBuilder();
+			for(int x : num) answer.append(x);
+			System.out.println(answer.toString());
+		}
+		else System.out.println("0");
+	}
+}
