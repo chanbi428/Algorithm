9fb78cbce4797dfdca6e13138d881485ebf6b7e5
Chanbi Bae <73926213+chanbi428@users.noreply.github.com>
2022-02-27 19:51:17 +0900

    [Silver V] Title: 수 이어가기, Time: 92 ms, Memory: 12472 KB -BaekjoonHub

---
+++ 백준/Silver/2635.수 이어가기/수 이어가기.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int first;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		first = Integer.parseInt(br.readLine());
+		
+		ArrayList<Integer> answer = new ArrayList<>();
+		ArrayList<Integer> tmp = new ArrayList<>();
+		int max = 0;
+		
+		for(int i=first/2; i<=first; i++) {
+			tmp = find(i);
+			if(max<=tmp.size()) {
+				max = tmp.size();
+				answer = tmp;
+			} else break;
+		}
+		
+		System.out.println(answer.size());
+		for(int x : answer) System.out.print(x+" ");
+	}
+	
+	static ArrayList<Integer> find(int n){
+		ArrayList<Integer> tmp = new ArrayList<>();
+		int pre = first;
+		int cur = n;
+		
+		while(true) {
+			if(pre<0) break;
+			tmp.add(pre);
+			pre = cur;
+			cur = tmp.get(tmp.size()-1)-cur;
+		}
+		
+		return tmp;
+	}
+}
