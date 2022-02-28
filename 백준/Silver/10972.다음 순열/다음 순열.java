04c95a4036a5d1222d8ab11f0232c88dff804e5c
Chanbi Bae <73926213+chanbi428@users.noreply.github.com>
2022-02-28 20:21:10 +0900

    [Silver III] Title: 다음 순열, Time: 312 ms, Memory: 18416 KB -BaekjoonHub

---
+++ 백준/Silver/10972.다음 순열/다음 순열.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int n;
+	static int[] num;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		num = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		if(np()) {
+			for(int x: num) {
+				System.out.print(x+" ");
+			}
+		} else System.out.println("-1");
+	}
+	static boolean np() {
+		
+		int i = n-1;
+		while(i>0 && num[i-1]>=num[i]) --i;
+		
+		if(i==0) return false;
+		
+		int j = n-1;
+		while(num[i-1]>=num[j]) --j;
+		
+		swap(i-1, j);
+		
+		int k = n-1;
+		while(i<k) swap(i++, k--);
+		
+		return true;
+	}
+	
+	static void swap(int a, int b) {
+		int tmp = num[a];
+		num[a] = num[b];
+		num[b] = tmp;
+	}
+}
