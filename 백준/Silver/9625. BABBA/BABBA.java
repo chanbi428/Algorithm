b7c068ebdb23cfef9e7798421729f0c06e7fc6ab
Chanbi Bae <73926213+chanbi428@users.noreply.github.com>
2022-03-11 20:10:19 +0900

    [Silver V] Title: BABBA, Time: 80 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Silver/9625. BABBA/BABBA.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());		
+		
+		int a = 1;
+		int b = 0;
+		
+		for(int i=0; i<n; i++) {
+			int tmp = a;
+			a = b;
+			b += tmp;
+		}
+		
+		System.out.println(a+" "+b);
+	}
+	
+}
