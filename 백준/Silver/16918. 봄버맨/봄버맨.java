58818bfd34074cb8a83977f69c75c8033884fcfa
Chanbi Bae <73926213+chanbi428@users.noreply.github.com>
2022-03-21 21:45:47 +0900

    [Silver I] Title: 봄버맨, Time: 252 ms, Memory: 20756 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.java
+++ 백준/Silver/16918. 봄버맨/봄버맨.java
@@ -5,6 +5,7 @@ public class Main {
 	
 	static int r, c, n;
 	static char[][] arr;
+	static int[][] bomb;
 	static int[] dx = {0, 0, -1, 1};
 	static int[] dy = {-1, 1, 0, 0};
 	
@@ -15,58 +16,58 @@ public class Main {
 		c = Integer.parseInt(st.nextToken());
 		n = Integer.parseInt(st.nextToken());
 		arr = new char[r][c];
-		
-		Queue<int[]> bomb = new ArrayDeque<>();  // 폭탄 있는 위치들 
-		Queue<int[]> empty = new ArrayDeque<>(); // 빈칸들 
+		bomb = new int[r][c];
 		
 		for(int i=0; i<r; i++) {
 			arr[i] = br.readLine().toCharArray();
 			for(int j=0; j<c; j++) {
-				if(arr[i][j]=='O') bomb.add(new int[] {i, j, 3});
-				else empty.add(new int[] {i, j});
+				if(arr[i][j]=='O') bomb[i][j] = 3;
 			}
 		}
 		
 		for(int sec=2; sec<=n; sec++) {
-			while(!empty.isEmpty()) {
-				int[] tmp = empty.poll();
-				arr[tmp[0]][tmp[1]] = 'O';
-				bomb.offer(new int[] {tmp[0], tmp[1], sec+3});
+			for(int i=0; i<r; i++) {
+				for(int j=0; j<c; j++) {
+					if(arr[i][j]=='.') {
+						arr[i][j] = 'O';
+						bomb[i][j] = sec+3;
+					}
+				}
 			}
 			sec++;
 			if(sec>n) break;
 			
-			Queue<int[]> bomb2 = new ArrayDeque<>();
 			boolean[][] visited = new boolean[r][c];
-			while(!bomb.isEmpty()) {
-				int[] tmp = bomb.poll();
-				if(tmp[2]!=sec) {
-					if(!visited[tmp[0]][tmp[1]]) bomb2.add(tmp);
-					continue;
-				}
-				if(!visited[tmp[0]][tmp[1]]) {
-					arr[tmp[0]][tmp[1]] = '.';
-					empty.add(new int[] {tmp[0], tmp[1]});
-					visited[tmp[0]][tmp[1]] = true;
-				}
-				for(int i=0; i<4; i++) {
-					int x = tmp[0]+dx[i];
-					int y = tmp[1]+dy[i];
-					if(x<0 || x>=r || y<0 || y>=c || visited[x][y]==true) continue;
-					arr[x][y] = '.';
-					empty.add(new int[] {x, y});
-					visited[x][y] = true;
+			for(int i=0; i<r; i++) {
+				for(int j=0; j<c; j++) {
+					if(bomb[i][j]==sec) {
+						visited[i][j] = true;
+						for(int k=0; k<4; k++) {
+							int x = i+dx[k];
+							int y = j+dy[k];
+							if(x<0 || x>=r || y<0 || y>=c) continue;
+							visited[x][y] = true;
+						}
+					}
 				}
 			}
-			bomb = bomb2;
+			for(int i=0; i<r; i++) {
+				for(int j=0; j<c; j++) {
+					if(visited[i][j]) {
+						arr[i][j] = '.';
+						bomb[i][j] = 0;
+					}
+				}
+			}
 		}
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i=0; i<r; i++) {
 			for(int j=0; j<c; j++) {
-				System.out.print(arr[i][j]);
+				sb.append(arr[i][j]);
 			}
-			System.out.println();
+			sb.append("\n");
 		}
-		
+		System.out.println(sb.toString());
 	}
 }
