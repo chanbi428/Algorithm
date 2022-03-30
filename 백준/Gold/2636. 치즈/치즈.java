85f2c306924359d75982e7589d5894733a988ab3
Chanbi Bae <73926213+chanbi428@users.noreply.github.com>
2022-03-30 10:15:11 +0900

    [Gold IV] Title: 치즈, Time: 116 ms, Memory: 13468 KB -BaekjoonHub

---
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	
+	static int n, m, sec, cnt;
+	static int[][] cheese;
+	//static boolean[][] visited;
+	//static Queue<int[]> Q = new ArrayDeque<>();
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		cheese = new int[n][m];
+		//visited = new boolean[n][m];
+		
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<m; j++) {
+				cheese[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int tmp = 0;
+		while(true) {
+			sec++;
+			tmp = air();
+			if(tmp==0) break;
+			cnt = tmp;
+		}
+		//melt();
+		System.out.println((sec-1)+"\n"+cnt);
+	}
+	
+	static int air() {
+		boolean[][] visited = new boolean[n][m];
+		visited[0][0] = true;
+		Queue<int[]> a = new ArrayDeque<>();
+		Queue<int[]> Q = new ArrayDeque<>();
+		a.offer(new int[] {0, 0});
+		while(!a.isEmpty()) {
+			int[] t = a.poll();
+			for(int i=0; i<4; i++) {
+				int x = t[0]+dx[i];
+				int y = t[1]+dy[i];
+				if(x<0 || x>=n || y<0 || y>=m || visited[x][y]) continue;
+				if(cheese[x][y]==0) a.offer(new int[] {x, y});
+				else {
+					Q.offer(new int[] {x, y, 1});
+					cheese[x][y] = 0;
+				}
+				visited[x][y] = true;
+			}
+		}
+		return Q.size();
+	}
+
+}
