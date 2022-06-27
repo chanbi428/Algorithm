6ebd2b1c9b9e7ac7d10234405738d858e6b781f9
Chanbi Bae <73926213+chanbi428@users.noreply.github.com>
2022-06-27 22:23:31 +0900

    [Gold III] Title: Brainf*ck, Time: 360 ms, Memory: 25976 KB -BaekjoonHub

--- 백준/Gold/2733. Brainf＊ck/Brainf＊ck.java
+++ 백준/Gold/2733. Brainf＊ck/Brainf＊ck.java
@@ -5,81 +5,99 @@ public class Main {
 	 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder answer = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 		
 		for(int t=1; t<=T; t++) {
-
-			StringBuilder lang = new StringBuilder();
+			StringBuilder message = new StringBuilder();
+			List<Integer> lang = new ArrayList<>();
 			Stack<Integer> stack = new Stack<>();
 			Map<Integer, Integer> map = new HashMap<>();
 			int[] brain = new int[32768];
-			boolean flag = false;
-			bw.write("PROGRAM #"+t+":\n");
 			
 			while(true) {
 				String str = br.readLine();
-				str.replaceAll(" ", "");
-				int remark = str.indexOf("%");
-				if(remark!=-1) str = str.substring(0, remark);
-				lang.append(str);
-				
 				if(str.equals("end")) break;
-			}
-		
-			for(int i=0; i<lang.length(); i++) {
-				if(lang.charAt(i)=='[') {
-					stack.add(i);
-				} else if(lang.charAt(i)==']') {
-					if(!stack.isEmpty()) {
-						int index = stack.pop();
-						map.put(index, i);
-						map.put(i, index);
-					}
-					else {
-						bw.write("COMPILE ERROR\n");
-						flag = true;
+				
+				if(message.length()!=0) continue;
+				for(int i=0; i<str.length(); i++) {
+					if(str.charAt(i)=='+') {
+						int cnt = 1;
+						while(true) {
+							if((i+cnt<str.length()) && str.charAt(i+cnt)=='+') cnt++;
+							else break;
+						}
+						lang.add(cnt);
+						i = i+cnt-1;
+						continue;
+					} else if(str.charAt(i)=='-') {
+						int cnt = 1;
+						while(true) {
+							if((i+cnt<str.length()) && str.charAt(i+cnt)=='-') cnt++;
+							else break;
+						}
+						lang.add(-cnt);
+						i = i+cnt-1;
+						continue;
+					} else if(str.charAt(i)=='<') {
+						lang.add(300);
+					} else if(str.charAt(i)=='>') {
+						lang.add(400);
+					} else if(str.charAt(i)=='.') {
+						lang.add(500);
+					} else if(str.charAt(i)=='[') {
+						lang.add(600);
+						stack.add(lang.size()-1);
+					} else if(str.charAt(i)==']') {
+						lang.add(700);
+						if(!stack.isEmpty()) {
+							int index = stack.pop();
+							map.put(index, lang.size()-1);
+							map.put(lang.size()-1, index);
+						}
+						else {
+							message.append("COMPILE ERROR");
+							break;
+						}
+					} else if(str.charAt(i)=='%') {
 						break;
 					}
 				}
 			}
 			
-			if(flag) continue;
-			if(!stack.isEmpty()) {
-				bw.write("COMPILE ERROR\n");
-				continue;
-			}
+			if(!stack.isEmpty()) message.append("COMPILE ERROR");
+			if(message.length()==0) {
 				int index = 0;
-				for(int i=0; i<lang.length(); i++) {
-					if(lang.charAt(i)=='+') {
-						brain[index]++;
-						if(brain[index]==256) brain[index] = 0;
-					} else if(lang.charAt(i)=='-') {
-						brain[index]--;
-						if(brain[index]==-1) brain[index] = 255;
-					} else if(lang.charAt(i)=='>') {
+				for(int i=0; i<lang.size(); i++) {
+					if(lang.get(i)<300) {
+						brain[index] += lang.get(i);
+						if(brain[index]>255) brain[index]-=256;
+						else if(brain[index]<0) brain[index]+=256;
+					} else if(lang.get(i)==300) {
 						index++;
 						if(index==32768) index=0;
-					} else if(lang.charAt(i)=='<') {
+					} else if(lang.get(i)==400) {
 						index--;
 						if(index==-1) index = 32767;
-					} else if(lang.charAt(i)=='.') {
-						bw.write((char)brain[index]);
-					} else if(lang.charAt(i)=='[') {
+					} else if(lang.get(i)==500) {
+						message.append((char)brain[index]);
+					} else if(lang.get(i)==600) {
 						if(brain[index]==0) {
 							i = map.get(i)-1;
 						}
-					} else if(lang.charAt(i)==']') {
+					} else if(lang.get(i)==700) {
 						if(brain[index]!=0) {
 							i = map.get(i)-1;
 						}
 					}
 				}
-		
-			bw.write("\n");
+			}
+			
+			answer.append("PROGRAM #"+t+":\n");
+			answer.append(message+"\n");
 		}
 		
-		bw.flush();
+		System.out.println(answer.toString());
 		
 	}
 
