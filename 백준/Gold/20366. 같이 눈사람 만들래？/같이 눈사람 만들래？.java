5b0c6c646ced49feb5021169c5e68f565fbdda66
Chanbi Bae <73926213+chanbi428@users.noreply.github.com>
2022-07-15 21:29:12 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 824 ms, Memory: 12280 KB -BaekjoonHub

---
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	 
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int answer = Integer.MAX_VALUE;
+		int[] snow = new int[n];
+		for(int i=0; i<n; i++) snow[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(snow);
+		for(int i=0; i<n; i++) {
+			for(int j=i+1; j<n; j++) {
+				int s = 0;
+				int e = n-1;
+				int tall = snow[i] + snow[j];
+				
+				while(s<e) {
+					if(s==i || s==j) {
+						s++;
+						continue;
+					}
+					if(e==j || e==i) {
+						e--;
+						continue;
+					}
+					int sum = snow[s] + snow[e];
+					int dif = Math.abs(tall-sum);
+					answer = Math.min(answer, dif);
+					
+					if(tall>sum) s++;
+					else e--;
+				}
+			}
+		}
+		
+		System.out.println(answer);
+	}
+
+}
