6014aad5eb01eb374b898815e110b6400ee21fed
Chanbi Bae <73926213+chanbi428@users.noreply.github.com>
2022-08-30 20:19:19 +0900

    [unrated] Title: 성격 유형 검사하기, Time: 1.96 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] survey, int[] choices) {
+        String answer = "";
+        
+        int[] score = new int[26];
+        
+        for(int i=0; i<survey.length; i++){
+            char disagree = survey[i].charAt(0);
+            char agree = survey[i].charAt(1);
+            
+            if(choices[i]<=3){
+                score[disagree-'A'] += (4-choices[i]);
+            } else{
+                score[agree-'A'] += (choices[i]-4);
+            }
+        }
+        
+        if(score['R'-'A']>=score['T'-'A']) answer += "R";
+        else answer += "T";
+        
+        if(score['C'-'A']>=score['F'-'A']) answer += "C";
+        else answer += "F";
+        
+        if(score['J'-'A']>=score['M'-'A']) answer += "J";
+        else answer += "M";
+        
+        if(score['A'-'A']>=score['N'-'A']) answer += "A";
+        else answer += "N";
+        
+        return answer;
+    }
+}
